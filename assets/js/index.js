5e67727a1b4c01fc4eb8bfd04dd0e1f3e7b4086b
Nikita Andrienko <nikikitaandrienko@gmail.com>
2024-11-04 13:38:04 +0200

    add up

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,20 @@
-const portfolioWrap = document.querySelector('.portfolio-wrap');
-const portfolioImage = document.querySelector('.img-fluid');
+const portfolioWrap = document.querySelector('.portfolio-wrap')
+const portfolioImage = document.querySelector('.img-fluid')
 
-portfolioWrap.addEventListener('mousemove', (e) => {
-  const rect = portfolioWrap.getBoundingClientRect();
-  const x = e.clientX - rect.left; // Положение курсора по оси X относительно контейнера
-  const y = e.clientY - rect.top;  // Положение курсора по оси Y относительно контейнера
+portfolioWrap.addEventListener('mousemove', e => {
+	const rect = portfolioWrap.getBoundingClientRect()
+	const x = e.clientX - rect.left // Положение курсора по оси X относительно контейнера
+	const y = e.clientY - rect.top // Положение курсора по оси Y относительно контейнера
 
-  // Рассчитываем центральную точку для масштабирования в зависимости от положения курсора
-  const moveX = ((x / rect.width) - 0.5) * 30;  // Коэффициент движения по оси X
-  const moveY = ((y / rect.height) - 0.5) * 30; // Коэффициент движения по оси Y
+	// Рассчитываем центральную точку для масштабирования в зависимости от положения курсора
+	const moveX = (x / rect.width - 0.5) * 30 // Коэффициент движения по оси X
+	const moveY = (y / rect.height - 0.5) * 30 // Коэффициент движения по оси Y
 
-  // Применяем трансформацию с увеличением и смещением изображения в зависимости от позиции курсора
-  portfolioImage.style.transform = `scale(1.5) translate(${moveX}px, ${moveY}px)`;
-});
+	// Применяем трансформацию с увеличением и смещением изображения в зависимости от позиции курсора
+	portfolioImage.style.transform = `scale(1.5) translate(${moveX}px, ${moveY}px)`
+})
 
 portfolioWrap.addEventListener('mouseleave', () => {
-  // Возвращаем изображение в исходное положение при уходе курсора
-  portfolioImage.style.transform = 'scale(1) translate(0, 0)';
-});
+	// Возвращаем изображение в исходное положение при уходе курсора
+	portfolioImage.style.transform = 'scale(1) translate(0, 0)'
+})
--- assets/js/tilt.js
+++ assets/js/tilt.js
@@ -601,3 +601,66 @@ var VanillaTilt = (function () {
 
 	return VanillaTilt
 })()
+
+VanillaTilt.prototype.extendSettings = function extendSettings(settings) {
+	var defaultSettings = {
+		reverse: false,
+		max: 15,
+		startX: 0,
+		startY: 0,
+		perspective: 1000,
+		easing: 'cubic-bezier(.03,.98,.52,.99)',
+		scale: 1.1, // Установите значение по умолчанию для масштаба
+		speed: 300,
+		transition: true,
+		axis: null,
+		glare: false,
+		'max-glare': 1,
+		'glare-prerender': false,
+		'full-page-listening': false,
+		'mouse-event-element': null,
+		reset: true,
+		'reset-to-start': true,
+		gyroscope: true,
+		gyroscopeMinAngleX: -45,
+		gyroscopeMaxAngleX: 45,
+		gyroscopeMinAngleY: -45,
+		gyroscopeMaxAngleY: 45,
+		gyroscopeSamples: 10,
+	}
+
+	// Остальной код остается без изменений...
+}
+
+VanillaTilt.prototype.update = function update() {
+	var values = this.getValues()
+
+	this.element.style.transform =
+		'perspective(' +
+		this.settings.perspective +
+		'px) ' +
+		'rotateX(' +
+		(this.settings.axis === 'x' ? 0 : values.tiltY) +
+		'deg) ' +
+		'rotateY(' +
+		(this.settings.axis === 'y' ? 0 : values.tiltX) +
+		'deg) ' +
+		'scale(' +
+		this.settings.scale +
+		')' // Примените масштаб
+
+	if (this.glare) {
+		this.glareElement.style.transform =
+			'rotate(' + values.angle + 'deg) translate(-50%, -50%)'
+		this.glareElement.style.opacity =
+			'' + (values.percentageY * this.settings['max-glare']) / 100
+	}
+
+	this.element.dispatchEvent(
+		new CustomEvent('tiltChange', {
+			detail: values,
+		})
+	)
+
+	this.updateCall = null
+}
