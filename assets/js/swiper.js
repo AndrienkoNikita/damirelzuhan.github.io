c1fe1bafa61b62b74afb3968b8e160a75f2388cc
Nikita Andrienko <nikikitaandrienko@gmail.com>
2025-03-04 14:13:05 +0200

    add update portfolio

---
+++ assets/js/swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+	loop: false,
+	simulateTouch: false,
+
+	// Если нужна пагинация
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Стрелки навигации
+
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+})
